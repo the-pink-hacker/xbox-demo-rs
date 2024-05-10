6f9185179d5a21c1193e8dd8f4093290b9c68934
Pink Garrett <pink@thepinkhacker.com>
2024-05-09 20:06:29 -0400

    Setup NXDK

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+#![feature(alloc_error_handler)]
+
+use core::panic::PanicInfo;
+
+#[macro_use]
+extern crate alloc;
+
+#[global_allocator]
+static ALLOCATOR: nxdk_rs::alloc::XboxKernelAlloc = nxdk_rs::alloc::XboxKernelAlloc {};
+
+#[alloc_error_handler]
+fn alloc_error_handler(layout: core::alloc::Layout) -> ! {
+    panic!("Allocation error: {:?}", layout);
+}
+
+#[no_mangle]
+pub extern "C" fn main() -> ! {
+    let message = format!("Hello from {}!", "RUST!!!");
+
+    nxdk_rs::hal::video::xvideo_set_mode(640, 480, 32, nxdk_rs::hal::video::RefreshRate::Default);
+    nxdk_rs::hal::debug::debug_print_str(&message);
+
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
